a735cccf6f65bebf5e5bab2d3957e507ce096e32
Patrick <yangsy9@mail2.sysu.edu.cn>
2018-12-17 10:29:00 +0800

    Initial commit

---
+++ MonoGame1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MonoGame1
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (var game = new MyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
